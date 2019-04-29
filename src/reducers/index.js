4b4018b76b366301655d3f25a13ccea6d86a2e2e
sunil maharjan <sunil.maharjan2@metropolia.fi>
2019-04-29 22:18:30 +0545

    initial commit

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '4:05' },
+        { title: 'Macarena', duration: '2:30' },
+        { title: 'All Stars', duration: '3:15' },
+        { title: 'I want it that way', duration: '5:45' }
+    ];
+};
